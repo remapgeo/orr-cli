9ab6c77cac41e4abb117c5785f7961b7ae9eaccd
Pedro Veloso <pedro.veloso@remapgeo.com>
2025-03-27 12:33:57 -0300

    Use environment when config file not present

--- orr_cli/config.py
+++ orr_cli/config.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 import psycopg2
 
@@ -18,19 +19,25 @@ TEMPLATES_DIR = f"{ROOT_DIR}/template"
 
 templets = Environment(loader=FileSystemLoader(searchpath=f"{ROOT_DIR}/template"))
 
+DB_NAME = getattr(settings, "DB_NAME", os.getenv("ORR_DB_NAME", "postgres"))
+DB_USER = getattr(settings, "DB_USER", os.getenv("ORR_DB_USER", "application"))
+DB_PASSWORD = getattr(
+    settings, "DB_PASSWORD", os.getenv("ORR_DB_PASSWORD", "application")
+)
+DB_HOST = getattr(settings, "DB_HOST", os.getenv("ORR_DB_HOST", "localhost"))
+DB_PORT = getattr(settings, "DB_PORT", os.getenv("ORR_DB_PORT", 5432))
+
 db_params = {
-    "dbname": settings.DB_NAME,
-    "user": settings.DB_USER,
-    "password": settings.DB_PASSWORD,
-    "host": settings.DB_HOST,
-    "port": settings.DB_PORT,
+    "dbname": DB_NAME,
+    "user": DB_USER,
+    "password": DB_PASSWORD,
+    "host": DB_HOST,
+    "port": DB_PORT,
 }
 
-db_url = f'postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
+db_url = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
-
-
 
 
 def get_db_connection():
@@ -39,5 +46,5 @@ def get_db_connection():
         user=settings.DB_USER,
         password=settings.DB_PASSWORD,
         host=settings.DB_HOST,
-        port=settings.DB_PORT
+        port=settings.DB_PORT,
     )
